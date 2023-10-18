307423a213ccbc055341bf77a8b74de1f3c6d094
LucaRemke <luca.remke@idsr.uni-stuttgart.de>
2023-10-18 17:59:06 +0200

    Add function printOwner

--- src/MyStatsPackage.jl
+++ src/MyStatsPackage.jl
@@ -2,9 +2,11 @@ module MyStatsPackage
 
 include("statistic_functions.jl")
 include("algebraic_functions.jl")
+include("printOwner.jl")
 
 export rse_sum, rse_mean, rse_std, rse_tstat
 export StatResult
 export rse_norm
+export printOwner
 
 end # module MyStatsPackage
